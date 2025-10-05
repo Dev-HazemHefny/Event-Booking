622de2d873090cf18a29ede2c533a0827a8c1b29
Dev-HazemHefny <hefnyhazem531@gmail.com>
2025-10-05 23:41:40 +0300

    ...

--- dbConnection.js
+++ dbConnection.js
@@ -2,6 +2,6 @@
 
 import mongoose from "mongoose";
 
-export const dbConnection = mongoose.connect("mongodb+srv://hefnyhazem531_db_user:Hazem1234@cluster0.lg7yltk.mongodb.net/?retryWrites=true&w=majority&appName=EventBooking")
+export const dbConnection = mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
