4372494b7ddc4a955d985a81f82f4be4311db6fe
Dev-HazemHefny <hefnyhazem531@gmail.com>
2025-10-12 23:57:01 +0300

    ..

--- GraphQl/schema.js
+++ GraphQl/schema.js
@@ -4,11 +4,15 @@ import userQueries from "./Queries/userQueries.js";
 import eventQueries from "./Queries/eventQueries.js";
 import { userMutations } from "./Mutaions/userMutations.js";
 
+
+//The RootQuery is the entry point for querying data.
+//  It's like the main menu of options you have when you want to retrieve information.
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: () => ({
     ...userQueries,
     ...eventQueries,
+    
   }),
 });
 
